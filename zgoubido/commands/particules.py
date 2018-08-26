f766ac5abf4d3243a572601063dd397fcc4ed3ab
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2018-08-26 17:00:05 +0200

    Particules all defined.

--- zgoubido/commands/particules.py
+++ zgoubido/commands/particules.py
@@ -1,7 +1,7 @@
 from .commands import Command
 
 
-class Particul(Command):
+class Particule(Command):
     KEYWORD = 'PARTICUL'
     PARAMETERS = {
         'M': 0,
@@ -11,43 +11,61 @@ class Particul(Command):
         'X': 0,
     }
 
-    def __init__(self, *params, **kwargs):
-        super().__init__(Particul.PARAMETERS, self.PARAMETERS, *params, **kwargs)
+    def __init__(self, name='', *params, **kwargs):
+        super().__init__(name, Particule.PARAMETERS, self.PARAMETERS, *params, **kwargs)
 
     def __str__(s):
         return f"""
-        '{s.KEYWORD}'
+        {super().__str__()}
         {s.M} {s.Q} {s.G} {s.tau} {s.X}
         """
 
 
-class Electron(Particul):
+class Electron(Particule):
     PARAMETERS = {
-        'M': 0.51099892e6,
+        'M': 0.51099892,
         'Q': -1.60217653e-19,
-        'G': (2.0023193043622 - 2) / 2,
+        'G': (-2.0023193043622 - 2) / 2,
     }
 
 
-class Positron(Particul):
+class Positron(Particule):
     PARAMETERS = {
-        'M': 0.51099892e6,
+        'M': 0.51099892,
         'Q': 1.60217653e-19,
-        'G': (2.0023193043622 - 2) / 2,
+        'G': (-2.0023193043622 - 2) / 2,
     }
 
 
-class Proton(Particul):
+class Muon(Particule):
     PARAMETERS = {
-        'M': 938.27203e6,
+        'M': 105.6583745,
+        'Q': -1.60217653e-19,
+        'G': (-2.0023318418 - 2) / 2,
+        'tau': 2.197029e-6,
+    }
+
+
+class AntiMuon(Particule):
+    PARAMETERS = {
+        'M': 105.6583745,
+        'Q': 1.60217653e-19,
+        'G': (-2.0023318418 - 2) / 2,
+        'tau': 2.197029e-6,
+    }
+
+
+class Proton(Particule):
+    PARAMETERS = {
+        'M': 938.27203,
         'Q': 1.602176487e-19,
         'G': (5.585694701 - 2) / 2,
     }
 
 
-class AntiProton(Particul):
+class AntiProton(Particule):
     PARAMETERS = {
-        'M': 938.27203e6,
+        'M': 938.27203,
         'Q': -1.602176487e-19,
         'G': (5.585694701 - 2) / 2,
     }
