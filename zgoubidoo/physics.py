ea28971191227390facf4e90f7b74501ec1a48e8
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2018-10-13 12:50:35 +0200

    Documenting the physics module and creating all the conversion variants.

--- zgoubidoo/physics.py
+++ zgoubidoo/physics.py
@@ -6,6 +6,13 @@ from . import ureg, Q_
 from .commands import Particule, Proton
 
 
+class ZgoubiPhysicsException(Exception):
+    """Exception raised for errors in the Zgoubidoo physics module."""
+
+    def __init__(self, m):
+        self.message = m
+
+
 class Kinematic:
     def __init__(self, q: Q_, particle: Particule=Proton()):
         self._q = q
@@ -70,27 +77,93 @@ def energy_to_momentum(e: Q_, particle: Particule=Proton()) -> Q_:
     >>> energy_to_momentum(230 * ureg.MeV).to('MeV_c')
     <Quantity(696.064029957015, 'megaelectronvolt_per_c')>
 
-    :param e:
-    :param particle:
-    :return:
+    :param e: kinetic energy
+    :param particle: the particle type (default: proton)
+    :return: relativistic momentum
     """
     return _np.sqrt((particle.mass * ureg.c**2 + e) ** 2 - particle.mass ** 2 * ureg.c ** 4) / ureg.c
 
 
 def energy_to_brho(e: Q_, particle: Particule=Proton()) -> Q_:
     """
-    Converts momentum to kinetic energy.
+    Converts kinetic energy to magnetic rigidity (brho).
 
     >>> energy_to_momentum(100 * ureg.MeV).to('MeV_c')
     <Quantity(444.58340724772927, 'megaelectronvolt_per_c')>
     >>> energy_to_momentum(230 * ureg.MeV).to('MeV_c')
     <Quantity(696.064029957015, 'megaelectronvolt_per_c')>
 
-    :param e:
-    :param particle:
-    :return:
+    :param e: kinetic energy
+    :param particle: the particle type (default: proton)
+    :return: magnetic rigidity
     """
     return momentum_to_brho(energy_to_momentum(e, particle), particle)
+
+
+def energy_to_range(e: Q_, particle: Particule=Proton()) -> Q_:
+    """
+    Converts kinetic energy to proton range in water; following IEC60601.
+
+    >>> energy_to_range(100 * ureg.MeV).to('cm')
+    <Quantity(7.7269636143698905, 'centimeter')>
+    >>> energy_to_range(230 * ureg.MeV).to('cm')
+    <Quantity(32.9424672323197, 'centimeter')>
+
+    :param e: kinetic energy
+    :param particle: the particle type (default: proton)
+    :return: proton range in water
+    """
+    if not isinstance(particle, Proton):
+        raise ZgoubiPhysicsException("Conversion to range only works for protons.")
+
+    b = 0.008539
+    c = 0.5271
+    d = 3.4917
+    e = e.to('MeV').magnitude
+    return _np.exp((-c + _np.sqrt(c ** 2 - 4 * b * (d - _np.log(e)))) / (2 * b)) * ureg.cm
+
+
+def energy_to_pv(e: Q_, particle: Particule=Proton()) -> Q_:
+    """
+    Converts kinetic energy to relativistic pv.
+
+    >>> energy_to_pv(230 * ureg.MeV)
+    <Quantity(414.71945005821937, 'megaelectronvolt')>
+
+    :param e: kinetic energy
+    :param particle: the particle type (default: proton)
+    :return: relativistic pv
+    """
+    return ((e + particle.mass * ureg.c**2)**2 - (particle.mass * ureg.c**2)**2) / (e + particle.mass * ureg.c**2)
+
+
+def energy_to_beta(e: Q_, particle: Particule=Proton()) -> float:
+    """
+    Converts the kinetic energy to relativistic beta.
+
+    >>> 1 + 1
+    2
+
+    :param e: kinetic energy
+    :param particle: the particle type (default: proton)
+    :return: relativistic beta
+    """
+    gamma = (particle.mass * ureg.c**2 + e) / (particle.mass * ureg.c**2)
+    return _np.sqrt((gamma ** 2 - 1) / gamma ** 2)
+
+
+def energy_to_gamma(e: Q_, particle: Particule=Proton()) -> float:
+    """
+    Converts the kinetic energy to relativistic gamma.
+
+    >>> 1 + 1
+    2
+
+    :param e: kinetic energy
+    :param particle: the particle type (default: proton)
+    :return: relativistic gamma
+    """
+    return (particle.mass * ureg.c**2 + e) / (particle.mass * ureg.c**2)
 
 
 def momentum_to_energy(p: Q_, particle: Particule=Proton()) -> Q_:
@@ -100,8 +173,8 @@ def momentum_to_energy(p: Q_, particle: Particule=Proton()) -> Q_:
     >>> momentum_to_energy(100 * ureg.MeV_c).to('MeV')
     <Quantity(5.313897343302641, 'megaelectronvolt')>
 
-    :param p:
-    :param particle:
+    :param p: relativistic momentum
+    :param particle: the particle type (default: proton)
     :return:
     """
     return _np.sqrt((p ** 2 * ureg.c ** 2) + ((particle.mass * ureg.c ** 2) ** 2)) - particle.mass * ureg.c ** 2
@@ -114,11 +187,25 @@ def momentum_to_brho(p: Q_, particle: Particule=Proton()) -> Q_:
     >>> momentum_to_brho(100 * ureg.MeV_c).to('tesla * meter')
     <Quantity(0.33356409519815206, 'meter * tesla')>
 
-    :param p:
-    :param particle:
+    :param p: relativistic momentum
+    :param particle: the particle type (default: proton)
     :return:
     """
     return p / particle.charge
+
+
+def momentum_to_range(p: Q_, particle: Particule=Proton()) -> Q_:
+    """
+    Converts momentum to proton range in water; following IEC60601.
+
+    >>> momentum_to_range(696 * ureg.MeV_c).to('cm')
+    <Quantity(32.93315610400117, 'centimeter')>
+
+    :param p: relativistic momentum
+    :param particle: the particle type (default: proton)
+    :return:
+    """
+    return energy_to_range(momentum_to_energy(p, particle), particle)
 
 
 def brho_to_momentum(brho: Q_, particle: Particule=Proton()) -> Q_:
@@ -128,50 +215,73 @@ def brho_to_momentum(brho: Q_, particle: Particule=Proton()) -> Q_:
     >>> momentum_to_brho(100 * ureg.MeV_c).to('tesla * meter')
     <Quantity(0.33356409519815206, 'meter * tesla')>
 
-    :param brho:
-    :param particle:
+    :param brho: magnetic rigidity
+    :param particle: the particle type (default: proton)
     :return:
     """
     return brho * particle.charge
 
 
-def energy_to_beta(ekin):
-    """Return beta relativistic from E [MeV/c^2] (proton)."""
-    gamma = (PROTON_MASS + ekin) / PROTON_MASS
-    return _np.sqrt((gamma ** 2 - 1) / gamma ** 2)
+def range_to_energy(r: Q_, particle: Particule=Proton()) -> Q_:
+    """
+    Converts proton range in water to kinetic energy following IEC60601.
+
+    >>> range_to_energy(32 * ureg.cm).to('MeV')
+    <Quantity(226.12911179644985, 'megaelectronvolt')>
+
+    :param r: proton range in water
+    :param particle: the particle type (default: proton)
+    :return:
+    """
+    if not isinstance(particle, Proton):
+        raise ZgoubiPhysicsException("Conversion from range only works for protons.")
+
+    a = 0.00169
+    b = -0.00490
+    c = 0.56137
+    d = 3.46405
+    r = r.to('cm').magnitude
+    return _np.exp(
+        a * _np.log(r) ** 3 + b * _np.log(r) ** 2 + c * _np.log(r) + d
+    ) * ureg.MeV
 
 
-def beta_to_gamma(beta):
-    """Return gamma relativistic from beta."""
+def beta_to_gamma(beta: float) -> float:
+    """
+    Converts relativistic beta to relativistic gamma.
+
+    >>> 1 + 1
+    2
+
+    :param beta: relativistic beta
+    :return:
+    """
     return 1/(_np.sqrt(1 - beta ** 2))
 
 
-def gamma_to_energy(gamma):
-    """Return relativistic energy from gamma."""
-    return gamma * PROTON_MASS - PROTON_MASS
+def beta_to_energy(beta: float, particle: Particule=Proton()) -> Q_:
+    """
+    Converts relativistic beta to kinetic energy.
+
+    >>> 1 + 1
+    2
+
+    :param beta: relativistic beta (dimensionless)
+    :param particle: the particle type (default: proton)
+    :return: kinetic energy
+    """
+    return beta_to_gamma(beta) * (particle.mass * ureg.c**2) - (particle.mass * ureg.c**2)
 
 
-def beta_to_energy(beta):
-    """Return relativistic energy from beta."""
-    return beta_to_gamma(beta) * PROTON_MASS - PROTON_MASS
+def gamma_to_energy(gamma: float, particle: Particule=Proton()) -> Q_:
+    """
+    Converts relativistic gamma to kinetic energy.
 
+    >>> gamma_to_energy(100.0).to('MeV')
+    <Quantity(92888.93096999999, 'megaelectronvolt')>
 
-def energy_to_pv(energy):
-    """Return relativistic factor 'pv' from kinetic energy (MeV)."""
-    E = energy + PROTON_MASS
-    return (E**2 - PROTON_MASS**2) / E
-
-
-def range_to_energy(r):
-    """Return the kinetic energy [MeV] from the range [g/cm^2]."""
-    a = 0.00169; b = -0.00490; c = 0.56137; d = 3.46405
-    return _np.exp(
-        a * _np.log(r) ** 3 + b * _np.log(r) ** 2 + c * _np.log(r) + d
-    )
-
-
-def energy_to_range(e):
-    """Return the range [g/cm^2] from the kinetic energy [MeV]."""
-    """IEC60601 energy to range in water"""
-    b = 0.008539; c = 0.5271; d = 3.4917
-    return _np.exp((-c + _np.sqrt(c ** 2 - 4 * b * (d - _np.log(e)))) / (2 * b))
+    :param gamma: relativistic gamma (dimensionless)
+    :param particle: the particle type (default: proton)
+    :return: kinetic energy
+    """
+    return gamma * (particle.mass * ureg.c**2) - (particle.mass * ureg.c**2)
