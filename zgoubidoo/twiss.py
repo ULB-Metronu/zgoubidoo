8c1f937a302b3b6ee1fa6bc15403ccdc193e693a
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2018-10-28 18:12:55 +0100

    Multiple bug fixes, doc and improvements in the Twiss module

--- zgoubidoo/twiss.py
+++ zgoubidoo/twiss.py
@@ -7,109 +7,117 @@ from .input import Input
 
 def compute_alpha_from_matrix(m: pd.DataFrame, twiss: pd.DataFrame, plane: int=1) -> pd.Series:
     """
-
-    :param m:
-    :param twiss:
-    :param plane:
-    :return:
+    Computes the Twiss alpha values at every steps of the input step-by-step transfer matrix.
+    :param m: the step-by-step transfer matrix for which the alpha values should be computed
+    :param twiss: the initial Twiss values
+    :param plane: an integer representing the plane (1 or 2)
+    :return: a Pandas Series with the alpha values computed at all steps of the input step-by-step transfer matrix
     """
     p = 1 if plane == 1 else 3
     v = 1 if plane == 1 else 2
-    R11: pd.Series = m[f"R{p}{p}"]
-    R12: pd.Series = m[f"R{p}{p+1}"]
-    R21: pd.Series = m[f"R{p+1}{p}"]
-    R22: pd.Series = m[f"R{p+1}{p+1}"]
-    ALPHA = twiss[f"ALPHA{v}{v}"]
-    BETA = twiss[f"BETA{v}{v}"]
-    GAMMA = twiss[f"GAMMA{v}{v}"]
-    return -R11 * R21 * BETA + (R11 * R22 + R12 * R21) * ALPHA - R12 * R22 * GAMMA
+    r11: pd.Series = m[f"R{p}{p}"]
+    r12: pd.Series = m[f"R{p}{p+1}"]
+    r21: pd.Series = m[f"R{p+1}{p}"]
+    r22: pd.Series = m[f"R{p+1}{p+1}"]
+    alpha: float = twiss[f"ALPHA{v}{v}"]
+    beta: float = twiss[f"BETA{v}{v}"]
+    gamma: float = twiss[f"GAMMA{v}{v}"]
+    return -r11 * r21 * beta + (r11 * r22 + r12 * r21) * alpha - r12 * r22 * gamma
 
 
-def compute_beta_from_matrix(m, twiss, plane=1) -> pd.Series:
+def compute_beta_from_matrix(m: pd.DataFrame, twiss, plane: int=1, strict: bool=False) -> pd.Series:
     """
-
-    :param m:
-    :param twiss:
-    :param plane:
-    :return:
+    Computes the Twiss beta values at every steps of the input step-by-step transfer matrix.
+    :param m: the step-by-step transfer matrix for which the beta values should be computed
+    :param twiss: the initial Twiss values
+    :param plane: an integer representing the plane (1 or 2)
+    :param strict: flag to activate the strict mode: checks and ensures that all computed beta are positive
+    :return: a Pandas Series with the beta values computed at all steps of the input step-by-step transfer matrix
     """
     p = 1 if plane == 1 else 3
     v = 1 if plane == 1 else 2
-    R11 = m[f"R{p}{p}"]
-    R12 = m[f"R{p}{p+1}"]
-    #R21 = m[f"R{p+1}{p}"]
-    #R22 = m[f"R{p+1}{p+1}"]
-    ALPHA = twiss[f"ALPHA{v}{v}"]
-    BETA = twiss[f"BETA{v}{v}"]
-    GAMMA = twiss[f"GAMMA{v}{v}"]
-    _ = R11**2 * BETA - 2.0 * R11 * R12 * ALPHA + R12**2 * GAMMA
-    #assert (_ > 0).all(), "Not all computed beta are positive."
+    r11: pd.Series = m[f"R{p}{p}"]
+    r12: pd.Series = m[f"R{p}{p+1}"]
+    alpha: float = twiss[f"ALPHA{v}{v}"]
+    beta: float = twiss[f"BETA{v}{v}"]
+    gamma: float = twiss[f"GAMMA{v}{v}"]
+    _ = np.square(r11) * beta - 2.0 * r11 * r12 * alpha + np.square(r12) * gamma
+    if strict:
+        assert (_ > 0).all(), "Not all computed beta are positive."
     return _
 
 
-def compute_gamma_from_matrix(m, twiss, plane=1) -> pd.Series:
+def compute_gamma_from_matrix(m: pd.DataFrame, twiss, plane: int=1) -> pd.Series:
     """
-
-    :param m:
-    :param twiss:
-    :param plane:
-    :return:
+    Computes the Twiss gamma values at every steps of the input step-by-step transfer matrix.
+    :param m: the step-by-step transfer matrix for which the beta values should be computed
+    :param twiss: the initial Twiss values
+    :param plane: an integer representing the plane (1 or 2)
+    :return: a Pandas Series with the gamma values computed at all steps of the input step-by-step transfer matrix
     """
     p = 1 if plane == 1 else 3
     v = 1 if plane == 1 else 2
-    #R11 = m[f"R{p}{p}"]
-    #R12 = m[f"R{p}{p+1}"]
-    R21 = m[f"R{p+1}{p}"]
-    R22 = m[f"R{p+1}{p+1}"]
-    ALPHA = twiss[f"ALPHA{v}{v}"]
-    BETA = twiss[f"BETA{v}{v}"]
-    GAMMA = twiss[f"GAMMA{v}{v}"]
-    return R21**2 * BETA - 2.0 * R21 * R22 * ALPHA + R22**2 * GAMMA
+    r21: pd.Series = m[f"R{p+1}{p}"]
+    r22: pd.Series = m[f"R{p+1}{p+1}"]
+    alpha: float = twiss[f"ALPHA{v}{v}"]
+    beta: float = twiss[f"BETA{v}{v}"]
+    gamma: float = twiss[f"GAMMA{v}{v}"]
+    return np.square(r21) * beta - 2.0 * r21 * r22 * alpha + np.square(r22) * gamma
 
 
-def compute_mu_from_matrix(m, twiss, plane=1) -> pd.Series:
+def compute_mu_from_matrix(m: pd.DataFrame, twiss, beta=None, plane: int=1) -> pd.Series:
     """
-
-    :param m:
-    :param twiss:
-    :param plane:
-    :return:
-    """
-    return 0.0
-
-
-def compute_jacobian_from_matrix(m, plane) -> pd.Series:
-    """
-
-    :param m:
-    :param plane:
-    :return:
+    Computes the phase advance values at every steps of the input step-by-step transfer matrix.
+    :param m: the step-by-step transfer matrix for which the beta values should be computed
+    :param twiss: the initial Twiss values
+    :param beta: pre-computed step-by-step beta values (to improve effiency if already compyted)
+    :param plane: an integer representing the plane (1 or 2)
+    :return: a Pandas Series with the phase advance computed at all steps of the input step-by-step transfer matrix
     """
     p = 1 if plane == 1 else 3
-    R11 = m[f"R{p}{p}"]
-    R12 = m[f"R{p}{p+1}"]
-    R21 = m[f"R{p+1}{p}"]
-    R22 = m[f"R{p+1}{p+1}"]
-    return R11 * R22 - R12 * R21
+    v = 1 if plane == 1 else 2
+    r11: pd.Series = m[f"R{p}{p}"]
+    r12: pd.Series = m[f"R{p}{p+1}"]
+    alpha0 = twiss[f"ALPHA{v}{v}"]
+    beta0 = twiss[f"BETA{v}{v}"]
+    if beta is None:
+        beta = compute_beta_from_matrix(m, twiss, plane)
+    return np.power(beta0 / beta, 0.5) * r11 - alpha0 * r12 / np.power(beta * beta0, 0.5)
+
+
+def compute_jacobian_from_matrix(m: pd.DataFrame, plane: int=1) -> pd.Series:
+    """
+    Computes the jacobian of the 2x2 transfer matrix (useful to verify the simplecticity).
+    :param m: the step-by-step transfer matrix for which the jacobians should be computed
+    :param plane: an integer representing the plane (1 or 2)
+    :return: a Pandas Series with the jacobian computed at all steps of the input step-by-step transfer matrix
+    """
+    p = 1 if plane == 1 else 3
+    r11: pd.Series = m[f"R{p}{p}"]
+    r12: pd.Series = m[f"R{p}{p+1}"]
+    r21: pd.Series = m[f"R{p+1}{p}"]
+    r22: pd.Series = m[f"R{p+1}{p+1}"]
+    return r11 * r22 - r12 * r21
 
 
 def compute_twiss(matrix: pd.DataFrame, twiss_init: pd.DataFrame) -> pd.DataFrame:
     """
-
-    :param matrix:
-    :param twiss_init:
-    :return:
+    Uses a step-by-step transfer matrix to compute the Twiss parameters (uncoupled). The phase advance and the
+    determinants of the jacobians are computed as well.
+    :param matrix: the input step-by-step transfer matrix
+    :param twiss_init: the initial values for the Twiss computation
+    :return: the same DataFrame as the input, but with added columns for the computed quantities.
     """
-    matrix['BETA11'] = compute_beta_from_matrix(matrix, twiss_init, 1)
-    matrix['BETA22'] = compute_beta_from_matrix(matrix, twiss_init, 2)
-    matrix['ALPHA11'] = compute_alpha_from_matrix(matrix, twiss_init, 1)
-    matrix['ALPHA22'] = compute_alpha_from_matrix(matrix, twiss_init, 2)
-    matrix['GAMMA11'] = compute_gamma_from_matrix(matrix, twiss_init, 1)
-    matrix['GAMMA22'] = compute_gamma_from_matrix(matrix, twiss_init, 2)
-    matrix['MU1'] = compute_mu_from_matrix(matrix, twiss_init, 1)
-    matrix['MU2'] = compute_mu_from_matrix(matrix, twiss_init, 2)
-    matrix['DET1'] = compute_jacobian_from_matrix(matrix, 1)
-    matrix['DET2'] = compute_jacobian_from_matrix(matrix, 2)
+    matrix['BETA11'] = compute_beta_from_matrix(matrix, twiss_init, plane=1)
+    matrix['BETA22'] = compute_beta_from_matrix(matrix, twiss_init, plane=2)
+    matrix['ALPHA11'] = compute_alpha_from_matrix(matrix, twiss_init, plane=1)
+    matrix['ALPHA22'] = compute_alpha_from_matrix(matrix, twiss_init, plane=2)
+    matrix['GAMMA11'] = compute_gamma_from_matrix(matrix, twiss_init, plane=1)
+    matrix['GAMMA22'] = compute_gamma_from_matrix(matrix, twiss_init, plane=2)
+    matrix['MU1'] = compute_mu_from_matrix(matrix, twiss_init, plane=1, beta=matrix['BETA11'])
+    matrix['MU2'] = compute_mu_from_matrix(matrix, twiss_init, plane=2, beta=matrix['BETA22'])
+    matrix['DET1'] = compute_jacobian_from_matrix(matrix, plane=1)
+    matrix['DET2'] = compute_jacobian_from_matrix(matrix, plane=2)
     return matrix
 
 
@@ -125,20 +133,23 @@ def align_tracks(tracks: pd.DataFrame,
 
     :param tracks: tracking data
     :param align_on: coordinates on which the tracks are aligned (typically 'X' or 'S')
-    :param identifier:
+    :param identifier: identifier of the column used for the particles indexing
     :param reference_track:
     :return:
     """
-    coordinates = ['Y-DY', 'T', 'Z', 'P', 'Yo', 'To', 'Zo', 'Po']
-    particules = ['O', 'A', 'C', 'E', 'G']
+    coordinates: list = ['Y-DY', 'T', 'Z', 'P', 'Yo', 'To', 'Zo', 'Po']  # Keep it in this order
+    particules: list = ['O', 'A', 'C', 'E', 'G', 'B', 'D', 'F', 'H']  # Keep it in this order
     ref: pd.DataFrame = tracks.query(f"{identifier} == '{reference_track}'")[coordinates + [align_on, 'LABEL1']]
     alignment_values = ref[align_on].values
+    assert np.all(np.diff(alignment_values) >= 0), "The reference alignment values are not monotonously increasing"
     data = np.zeros((len(particules), alignment_values.shape[0], len(coordinates)))
     data[0, :, :] = ref[coordinates].values
     for i, p in enumerate(particules[1:]):
         particule = tracks.query(f"{identifier} == '{p}'")
         for j, c in enumerate(coordinates):
             try:
+                assert np.all(np.diff(particule[align_on].values) >= 0), \
+                    "The alignment values are not monotonously increasing"
                 data[i+1, :, j] = np.interp(alignment_values, particule[align_on].values, particule[c].values)
             except ValueError:
                 pass
@@ -148,35 +159,36 @@ def align_tracks(tracks: pd.DataFrame,
 
 def compute_transfer_matrix(beamline: Input, tracks: pd.DataFrame, align_on: str='X') -> pd.DataFrame:
     """
-    Constructs the step-by-step transfer matrix from tracking data (finite differences).
+    Constructs the step-by-step transfer matrix from tracking data (finite differences). The approximation
+    uses the O(3) formula (not just the O(1) formula) and therefore makes use of all the particles.
     :param beamline: the Zgoubidoo Input beamline
     :param tracks: tracking data
     :param align_on: coordinates on which the tracks are aligned (typically 'X' or 'S')
     :return: a Panda DataFrame representing the transfer matrix
     """
     elements = tracks.LABEL1.unique()
-    offset = 0
+    offset: float = 0
     matrix = pd.DataFrame()
     for e in beamline.line:
         if e.LABEL1 not in elements:
             if isinstance(e, Patchable):
-                offset += (e.exit.x - e.entry.x).to('cm').magnitude
+                offset += (e.exit.x - e.entry.x).to('m').magnitude if align_on != 'S' else 0.0
             continue
         t = tracks[tracks.LABEL1 == e.LABEL1]
         data, ref = align_tracks(t, align_on=align_on)
-        n = 4
-        normalization = [
-            data[i + 1, :, i + n] - data[0, :, i + n] for i in range(0, n)
-        ]
+        n_dimensions: int = 4
+        normalization = [2 * (data[i + 1, :, i + n_dimensions] - data[0, :, i + n_dimensions])
+                         for i in range(0, n_dimensions)
+                         ]
         m = pd.DataFrame(
             {
-                f"R{i+1}{j+1}": pd.Series((data[i + 1, :, j] - data[0, :, j]) / normalization[i])
-                #f"R{i + 1}{j + 1}": pd.Series(data[i + 1, :, j])
-                for i in range(0, n)
-                for j in range(0, n)
+                f"R{j+1}{i+1}": pd.Series((data[i + 1, :, j] - data[i + 5, :, j] - data[0, :, j]) / normalization[i])
+                for i in range(0, n_dimensions)
+                for j in range(0, n_dimensions)
             }
         )
-        m[align_on] = ref[align_on].values + offset
+        m['X'] = ref[align_on].values + offset
+        m['S'] = ref[align_on].values + offset
         m['LABEL1'] = ref['LABEL1']
         matrix = matrix.append(m)
         offset += ref[align_on].max() if align_on != 'S' else 0.0
