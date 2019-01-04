397bf475e8ff4d9ccac8abd7e9d37e6bcace85d9
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2019-01-04 09:41:42 +0100

    Doc update.

--- zgoubidoo/sequence/sequence.py
+++ zgoubidoo/sequence/sequence.py
@@ -41,24 +41,31 @@ class Coordinates:
         1.0
     """
     y: float = 0
+    """Horizontal plane coordinate."""
     t: float = 0
+    """Horizontal plane angle."""
     z: float = 0
+    """Vertical plane coordinate"""
     p: float = 0
+    """Vertical plane angle."""
     x: float = 0
+    """Longitudinal coordinate."""
     d: float = 1
+    """Off-momentum offset"""
     iex: int = 1
+    """Particle alive status."""
 
     def __getitem__(self, item: int):
         return getattr(self, list(self.__dataclass_fields__.keys())[item])
 
     @property
     def array(self) -> _np.array:
-        """Convert to a numpy array."""
+        """Provides a numpy array."""
         return _np.array(self.list)
 
     @property
     def list(self) -> list:
-        """Convert to a flat list."""
+        """Provides a flat list."""
         return list(self.__dict__.values())
 
 
