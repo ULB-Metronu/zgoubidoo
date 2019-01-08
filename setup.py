8e8adaa7a44006ce51ff982db634ec59f85df4d0
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2019-01-08 09:18:42 +0100

    Minor change in the setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
+"""Installation with setuptools or pip."""
 from setuptools import setup, find_packages
 import os
 import ast
 
 
 def get_version_from_init():
+    """Obtain library version from main init."""
     init_file = os.path.join(
         os.path.dirname(__file__), 'zgoubidoo', '__init__.py'
     )
-    with open(init_file, 'r') as fd:
+    with open(init_file) as fd:
         for line in fd:
             if line.startswith('__version__'):
                 return ast.literal_eval(line.split('=', 1)[1].strip())
