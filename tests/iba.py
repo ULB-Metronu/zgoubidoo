a3e9c0554133cbb821e335126ba05e729a6ec021
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2019-01-25 09:36:20 +0100

    Minor

--- tests/iba.py
+++ tests/iba.py
@@ -11,6 +11,13 @@ cgtr = CGTR(b3g=B3G(magnet_opening=70 * _ureg.degree,
                     exit_fringe_lambda=9 * _ureg.cm,
                     ),
             )
+cgtr.q1g.current = 1
+cgtr.q2g.current = 1
+cgtr.q3g.current = 1
+cgtr.q4g.current = 1
+cgtr.q5g.current = 1
+cgtr.q6g.current = 1
+cgtr.q7g.current = 1
 spots = cgtr.spots([(0.0, 0.0), (-20.0, 2.0), (20.0, 2.0)])
 fig = cgtr.plot()
 fig.show()
