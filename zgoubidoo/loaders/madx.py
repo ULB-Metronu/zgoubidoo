0f40099b4de33c2f6b13aede256d2e308cc681e2
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2019-02-16 15:06:36 +0100

    Fix bug in MAD-X conversion.

--- zgoubidoo/loaders/madx.py
+++ zgoubidoo/loaders/madx.py
@@ -1,33 +1,130 @@
 """
 TODO
 """
-from typing import Optional
+from typing import Optional, Dict
 import os
+import sys
 import pandas as pd
 import numpy as np
 from .. import ureg as _ureg
 from ..physics import Sequence as _Sequence
-from ..commands import *
+from ..commands import Dipole, Quadrupole, Sextupole, Octupole, Command, Marker, Drift, Bend
 from ..kinematics import Kinematics
-
-MADX_ELEMENTS = {
-    'MARKER': lambda r, _, __: Marker(r.name[0:8]),
-    'DRIFT': lambda r, _, __: Drift(r.name[0:8], XL=r['L'] * _ureg.meter),
-    'SBEND': lambda r, k, o: Dipole(r.name[0:8],
-                                    AT=np.abs(r['ANGLE'] * _ureg.radian),
-                                    RM=np.abs(r['L'] / r['ANGLE'] * _ureg.meter),
-                                    B0=k.brho / (r['L'] / r['ANGLE'] * _ureg.meter),
-                                    ),
-    'QUADRUPOLE': lambda r, k, o: Quadrupole(r.name[0:8],
-                                             XL=r['L'] * _ureg.meter,
-                                             R0=o.get('R0', 10 * _ureg.cm),
-                                             B0=r['K1L'] / r['L'] * k.brho_ * o.get('R0', 10) * _ureg.tesla,
-                                             ),
-    'SEXTUPOLE': lambda r, k, o: Sextupole(r.name[0:8], XL=r['L'] * _ureg.meter),
-}
+from ..commands import particules
 
 
-def load_madx_twiss_headers(filename: str, path: str = '.') -> pd.Series:
+def create_madx_marker(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> Command:
+    """
+
+    Args:
+        twiss_row:
+        kinematics:
+        options:
+
+    Returns:
+
+    """
+    return Marker(twiss_row.name[0:8])
+
+
+def create_madx_drift(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> Command:
+    """
+
+    Args:
+        twiss_row:
+        kinematics:
+        options:
+
+    Returns:
+
+    """
+    return options.get('drift_command', Drift)(twiss_row.name[0:8], XL=twiss_row['L'] * _ureg.meter)
+
+
+def create_madx_rbend(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> Command:
+    """
+
+    Args:
+        twiss_row:
+        kinematics:
+        options:
+
+    Returns:
+
+    """
+    return options.get('rbend_command', Bend)(twiss_row.name[0:8],
+                                              XL=twiss_row['L'] * _ureg.meter,
+                                              B0=kinematics.brho / (twiss_row['L'] / twiss_row['ANGLE'] * _ureg.meter),
+                                              )
+
+
+def create_madx_sbend(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> Command:
+    """
+
+    Args:
+        twiss_row:
+        kinematics:
+        options:
+
+    Returns:
+
+    """
+    radius = np.abs(twiss_row['L'] / twiss_row['ANGLE']) * _ureg.meter
+    d = options.get('sbend_command', Dipole)(twiss_row.name[0:8],
+                                             AT=np.abs(twiss_row['ANGLE']) * _ureg.radian,
+                                             RM=radius,
+                                             B0=kinematics.brho / radius,
+                                             )
+    return d
+
+
+def create_madx_quadrupole(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> Command:
+    """
+
+    Args:
+        twiss_row:
+        kinematics:
+        options:
+
+    Returns:
+
+    """
+    return Quadrupole(twiss_row.name[0:8],
+                      XL=twiss_row['L'] * _ureg.meter,
+                      R0=options.get('R0', 10 * _ureg.cm),
+                      B0=twiss_row['K1L'] / twiss_row['L'] * kinematics.brho_ * options.get('R0', 0.1) * _ureg.tesla,
+                      )
+
+
+def create_madx_sextupole(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> Command:
+    """
+
+    Args:
+        twiss_row:
+        kinematics:
+        options:
+
+    Returns:
+
+    """
+    return Sextupole(twiss_row.name[0:8], XL=twiss_row['L'] * _ureg.meter)
+
+
+def create_madx_octupole(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> Command:
+    """
+
+    Args:
+        twiss_row:
+        kinematics:
+        options:
+
+    Returns:
+
+    """
+    return Octupole(twiss_row.name[0:8], XL=twiss_row['L'] * _ureg.meter)
+
+
+def load_madx_twiss_headers(filename: str = 'twiss.outx', path: str = '.') -> pd.Series:
     """
 
     Args:
@@ -47,7 +144,7 @@ def load_madx_twiss_headers(filename: str, path: str = '.') -> pd.Series:
                        )[0:46]
 
 
-def load_madx_twiss_table(filename: str, path: str = '.') -> pd.DataFrame:
+def load_madx_twiss_table(filename: str = 'twiss.outx', path: str = '.') -> pd.DataFrame:
     """
 
     Args:
@@ -99,12 +196,12 @@ def load_madx_twiss_table(filename: str, path: str = '.') -> pd.DataFrame:
     return _
 
 
-def from_madx_twiss(filename: str,
+def from_madx_twiss(filename: str = 'twiss.outx',
                     path: str = '.',
                     options: Optional[dict] = None,
                     converters: Optional[dict] = None) -> _Sequence:
     """
-
+    TODO
     Args:
         filename:
         path:
@@ -114,18 +211,22 @@ def from_madx_twiss(filename: str,
     Returns:
 
     """
-    conversion_functions = {**MADX_ELEMENTS, **(converters or {})}
+    madx_converters = {k.split('_')[2].upper(): getattr(sys.modules[__name__], k)
+                       for k in globals().keys() if k.startswith('create_madx')}
+    conversion_functions = {**madx_converters, **(converters or {})}
     options = options or {}
     twiss_headers = load_madx_twiss_headers(filename, path)
-    k = Kinematics(float(twiss_headers['PC']) * _ureg.GeV_c)
+    p = getattr(particules, twiss_headers['PARTICLE'].capitalize())
+    k = Kinematics(float(twiss_headers['PC']) * _ureg.GeV_c, particle=p)
     twiss_table = list(
                       load_madx_twiss_table(filename, path).set_index('NAME').apply(
-                          lambda _: conversion_functions[_['KEYWORD']](_, k, options.get(_['KEYWORD'], {})),
+                          lambda _: conversion_functions.get(_['KEYWORD'],
+                                                             lambda _, __, ___: None
+                                                             )(_, k, options.get(_['KEYWORD'], {})),
                           axis=1
                       ).values
-                  )
+    )
     return _Sequence(name=twiss_headers['NAME'],
                      sequence=twiss_table,
                      metadata=twiss_headers,
-                     particle=getattr(zgoubidoo.commands, twiss_headers['PARTICLE'].capitalize())
-                     )
+                     particle=p)
