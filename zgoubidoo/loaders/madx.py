c22dcbf6f0a6d066eeba1b79a480295c71b9aceb
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2019-02-25 15:36:48 +0100

    MAD-X loader.

--- zgoubidoo/loaders/madx.py
+++ zgoubidoo/loaders/madx.py
@@ -9,12 +9,116 @@ import numpy as np
 import itertools
 from .. import ureg as _ureg
 from ..sequence import Sequence as _Sequence
-from ..commands import Dipole, Quadrupole, Sextupole, Octupole, Command, Marker, Drift, Bend, Ymy
+from ..commands import Dipole, Quadrupole, Sextupole, Octupole, Command, Marker, Drift, Bend, Ymy, ChangeRef, Multipole
 from ..kinematics import Kinematics
 from ..commands import particules
+from ..units import _m
+
+MADX_TWISS_HEADERS = [
+    'NAME',
+    'KEYWORD',
+    'S',
+    'BETX', 'ALFX', 'MUX',
+    'BETY', 'ALFY', 'MUY',
+    'X', 'PX', 'Y', 'PY', 'T', 'PT',
+    'DX', 'DPX', 'DY', 'DPY',
+    'WX',
+    'PHIX',
+    'DMUX',
+    'WY',
+    'PHIY',
+    'DMUY',
+    'DDX', 'DDPX',
+    'DDY', 'DDPY',
+    'R11', 'R12', 'R21', 'R22',
+    'ENERGY',
+    'L',
+    'ANGLE',
+    'K0L', 'K0SL',
+    'K1L', 'K1SL',
+    'K2L', 'K2SL',
+    'K3L', 'K3SL',
+    'K4L', 'K4SL',
+    'K5L', 'K5SL',
+    'K6L', 'K6SL',
+    'K7L', 'K7SL',
+    'K8L', 'K8SL',
+    'K9L', 'K9SL',
+    'K10L', 'K10SL',
+    'K11L', 'K11SL',
+    'K12L', 'K12SL',
+    'K13L', 'K13SL',
+    'K14L', 'K14SL',
+    'K15L', 'K15SL',
+    'K16L', 'K16SL',
+    'K17L', 'K17SL',
+    'K18L', 'K18SL',
+    'K19L', 'K19SL',
+    'K20L', 'K20SL',
+    'KSI',
+    'HKICK', 'VKICK',
+    'TILT',
+    'E1', 'E2',
+    'H1', 'H2',
+    'HGAP',
+    'FINT',
+    'FINTX',
+    'VOLT',
+    'LAG',
+    'FREQ',
+    'HARMON',
+    'SLOT_ID',
+    'ASSEMBLY_ID',
+    'MECH_SEP',
+    'V_POS',
+    'LRAD',
+    'PARENT',
+    'RE11', 'RE12', 'RE13', 'RE14', 'RE15', 'RE16',
+    'RE21', 'RE22', 'RE23', 'RE24', 'RE25', 'RE26',
+    'RE31', 'RE32', 'RE33', 'RE34', 'RE35', 'RE36',
+    'RE41', 'RE42', 'RE43', 'RE44', 'RE45', 'RE46',
+    'RE51', 'RE52', 'RE53', 'RE54', 'RE55', 'RE56',
+    'RE61', 'RE62', 'RE63', 'RE64', 'RE65', 'RE66',
+    'KMAX',
+    'KMIN',
+    'CALIB',
+    'POLARITY',
+    'ALFA',
+    'BETA11', 'BETA12', 'BETA13',
+    'BETA21', 'BETA22', 'BETA23',
+    'BETA31', 'BETA32', 'BETA33',
+    'ALFA11', 'ALFA12', 'ALFA13',
+    'ALFA21', 'ALFA22', 'ALFA23',
+    'ALFA31', 'ALFA32', 'ALFA33',
+    'GAMA11', 'GAMA12', 'GAMA13',
+    'GAMA21', 'GAMA22', 'GAMA23',
+    'GAMA31', 'GAMA32', 'GAMA33',
+    'BETA11P', 'BETA12P', 'BETA13P',
+    'BETA21P', 'BETA22P', 'BETA23P',
+    'BETA31P', 'BETA32P', 'BETA33P',
+    'ALFA11P', 'ALFA12P', 'ALFA13P',
+    'ALFA21P', 'ALFA22P', 'ALFA23P',
+    'ALFA31P', 'ALFA32P', 'ALFA33P',
+    'GAMA11P', 'GAMA12P', 'GAMA13P',
+    'GAMA21P', 'GAMA22P', 'GAMA23P',
+    'GAMA31P', 'GAMA32P', 'GAMA33P',
+    'DISP1', 'DISP2', 'DISP3', 'DISP4',
+    'DISP1P', 'DISP2P', 'DISP3P', 'DISP4P',
+    'DISP1P2', 'DISP2P2', 'DISP3P2', 'DISP4P2',
+    'DISP1P3', 'DISP2P3', 'DISP3P3', 'DISP4P3',
+    'MU1', 'MU2', 'MU3',
+    'SIG11', 'SIG12', 'SIG13', 'SIG14', 'SIG15', 'SIG16',
+    'SIG21', 'SIG22', 'SIG23', 'SIG24', 'SIG25', 'SIG26',
+    'SIG31', 'SIG32', 'SIG33', 'SIG34', 'SIG35', 'SIG36',
+    'SIG41', 'SIG42', 'SIG43', 'SIG44', 'SIG45', 'SIG46',
+    'SIG51', 'SIG52', 'SIG53', 'SIG54', 'SIG55', 'SIG56',
+    'SIG61', 'SIG62', 'SIG63', 'SIG64', 'SIG65', 'SIG66',
+    'N1',
+]
+"""MAD-X Twiss headers (by default, when all columns are selected)."""
 
 
-def create_madx_marker(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> Command:
+def create_madx_marker(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> List[Command]:
     """
     Create a Marker command from the equivalent MAD-X marker keyword.
 
@@ -26,7 +130,7 @@ def create_madx_marker(twiss_row: pd.Series, kinematics: Kinematics, options: Di
     Returns:
 
     """
-    return [options.get('command', Marker)()]
+    return [options.get('command', Marker)().generate_label(prefix=twiss_row.name[0:8])]
 
 
 def create_madx_drift(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> List[Command]:
@@ -40,7 +144,7 @@ def create_madx_drift(twiss_row: pd.Series, kinematics: Kinematics, options: Dic
     Returns:
 
     """
-    return [options.get('command', Drift)(XL=twiss_row['L'] * _ureg.meter)]
+    return [options.get('command', Drift)(XL=twiss_row['L'] * _ureg.meter).generate_label(prefix=twiss_row.name[0:8])]
 
 
 def create_madx_rbend(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> List[Command]:
@@ -55,9 +159,34 @@ def create_madx_rbend(twiss_row: pd.Series, kinematics: Kinematics, options: Dic
 
     """
     return [options.get('command', Bend)(twiss_row.name[0:8],
-                                              XL=twiss_row['L'] * _ureg.meter,
-                                              B0=kinematics.brho / (twiss_row['L'] / twiss_row['ANGLE'] * _ureg.meter),
-                                              )]
+                                         XL=twiss_row['L'] * _ureg.meter,
+                                         B0=kinematics.brho / (twiss_row['L'] / twiss_row['ANGLE'] * _ureg.meter),
+                                         )]
+
+
+def create_madx_sbend_with_multipole(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> List[Command]:
+    """
+
+    Args:
+        twiss_row:
+        kinematics:
+        options:
+
+    Returns:
+
+    """
+    bore_radius = options.get('R0', 10 * _ureg.cm)
+    return [
+        Multipole(
+            XL=twiss_row['L'] * _ureg.meter,
+            R0=bore_radius,
+            B1=kinematics.brho / (twiss_row['L'] / twiss_row['ANGLE'] * _ureg.m),
+            B2=twiss_row['K1L'] / twiss_row['L'] * kinematics.brho_ * _m(bore_radius) * _ureg.tesla,
+            R1=twiss_row['TILT'] * _ureg.radian,
+            R2=twiss_row['TILT'] * _ureg.radian,
+            KPOS=3,  # MAD-X convention
+        ).generate_label(prefix=twiss_row.name[0:8]),
+    ]
 
 
 def create_madx_sbend(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> List[Command]:
@@ -71,17 +200,31 @@ def create_madx_sbend(twiss_row: pd.Series, kinematics: Kinematics, options: Dic
     Returns:
 
     """
-    radius = np.abs(twiss_row['L'] / twiss_row['ANGLE']) * _ureg.meter
-    d = options.get('command', Dipole)(twiss_row.name[0:8],
-                                       AT=np.abs(twiss_row['ANGLE']) * _ureg.radian,
-                                       RM=radius,
-                                       B0=kinematics.brho / radius,
-                                       THETA_E=float(twiss_row['E1']) * _ureg.radian,
-                                       THETA_S=float(twiss_row['E2']) * _ureg.radian,
-                                       )
-    if twiss_row['ANGLE'] < 0:
-        return [Ymy(), d]
-    return [d]
+    if options.get('command', Dipole) is Multipole:
+        return create_madx_sbend_with_multipole(twiss_row, kinematics, options)
+    else:
+        radius = np.abs(twiss_row['L'] / twiss_row['ANGLE']) * _ureg.meter
+        b0 = kinematics.brho / radius
+        d = Dipole(AT=np.abs(twiss_row['ANGLE']) * _ureg.radian,
+                   RM=radius,
+                   B0=b0,
+                   THETA_E=-twiss_row['E1'] * _ureg.radian,
+                   THETA_S=-twiss_row['E2'] * _ureg.radian,
+                   ).generate_label(prefix=twiss_row.name[0:8])
+        if twiss_row['ANGLE'] < 0:
+            return [
+                ChangeRef(TRANSFORMATIONS=[['XR', float(twiss_row['TILT']) * _ureg.radian]]),
+                Ymy(),
+                d,
+                Ymy(),
+                ChangeRef(TRANSFORMATIONS=[['XR', -float(twiss_row['TILT']) * _ureg.radian]]),
+            ]
+        else:
+            return [
+                ChangeRef(TRANSFORMATIONS=[['XR', float(twiss_row['TILT']) * _ureg.radian]]),
+                d,
+                ChangeRef(TRANSFORMATIONS=[['XR', -float(twiss_row['TILT']) * _ureg.radian]]),
+            ]
 
 
 def create_madx_quadrupole(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> List[Command]:
@@ -95,15 +238,16 @@ def create_madx_quadrupole(twiss_row: pd.Series, kinematics: Kinematics, options
     Returns:
 
     """
-    return [Quadrupole(twiss_row.name[0:8],
-                      XL=twiss_row['L'] * _ureg.meter,
-                      R0=options.get('R0', 10 * _ureg.cm),
-                      B0=twiss_row['K1L'] / twiss_row['L'] * kinematics.brho_ *
-                         options.get('R0', 10 * _ureg.cm).to('m').magnitude * _ureg.tesla,
-                      )]
+
+    bore_radius = options.get('R0', 10 * _ureg.cm)
+    return [Quadrupole(XL=twiss_row['L'] * _ureg.meter,
+                       R0=bore_radius,
+                       B0=twiss_row['K1L'] / twiss_row['L'] * kinematics.brho_* _m(bore_radius) * _ureg.tesla,
+                       ).generate_label(prefix=twiss_row.name[0:8])
+            ]
 
 
-def create_madx_sextupole(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> Command:
+def create_madx_sextupole(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> List[Command]:
     """
 
     Args:
@@ -117,7 +261,7 @@ def create_madx_sextupole(twiss_row: pd.Series, kinematics: Kinematics, options:
     return [Sextupole(twiss_row.name[0:8], XL=twiss_row['L'] * _ureg.meter)]
 
 
-def create_madx_octupole(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> Command:
+def create_madx_octupole(twiss_row: pd.Series, kinematics: Kinematics, options: Dict) -> List[Command]:
     """
 
     Args:
@@ -151,45 +295,19 @@ def load_madx_twiss_headers(filename: str = 'twiss.outx', path: str = '.') -> pd
                        )[0:46]
 
 
-def load_madx_twiss_table(filename: str = 'twiss.outx', path: str = '.') -> pd.DataFrame:
+def load_madx_twiss_table(filename: str = 'twiss.outx', path: str = '.', headers: List = None) -> pd.DataFrame:
     """
 
     Args:
         filename:
         path:
+        headers:
 
     Returns:
 
     """
-    headers = [
-        'NAME',
-        'KEYWORD',
-        'S',
-        'BETX', 'ALFX', 'MUX',
-        'BETY', 'ALFY', 'MUY',
-        'X', 'PX', 'Y', 'PY', 'T', 'PT',
-        'DX', 'DPX', 'DY', 'DPY',
-        'PHIX',
-        'DMUX',
-        'PHIY',
-        'DMUY',
-        'DDX',
-        'DDPX',
-        'DDY',
-        'DDPY',
-        'K1L', 'K2L', 'K3L', 'K4L', 'K5L', 'K6L',
-        'K1SL', 'K2SL', 'K3SL', 'K4SL', 'K5SL', 'K6SL',
-        'ENERGY',
-        'L',
-        'ANGLE',
-        'HKICK', 'VKICK',
-        'TILT',
-        'E1', 'E2', 'H1', 'H2',
-        'HGAP', 'FINT', 'FINTX',
-        'KSI',
-        'APERTYPE', 'APER_1', 'APER_2',
-    ]
-    _ = pd \
+    headers = headers or MADX_TWISS_HEADERS
+    _: pd.DataFrame = pd \
         .read_csv(os.path.join(path, filename),
                   skiprows=47,
                   sep=r'\s+',
@@ -197,14 +315,17 @@ def load_madx_twiss_table(filename: str = 'twiss.outx', path: str = '.') -> pd.D
                   names=headers,
                   ) \
         .drop(0)
-    _['L'] = _['L'].apply(float)
-    _['ANGLE'] = _['ANGLE'].apply(float)
-    _['K1L'] = _['K1L'].apply(float)
+    for c in _.columns:
+        try:
+            _[c] = _[c].apply(float)
+        except ValueError:
+            pass
     return _
 
 
 def from_madx_twiss(filename: str = 'twiss.outx',
                     path: str = '.',
+                    headers: List = None,
                     options: Optional[dict] = None,
                     converters: Optional[dict] = None) -> _Sequence:
     """
@@ -212,6 +333,7 @@ def from_madx_twiss(filename: str = 'twiss.outx',
     Args:
         filename:
         path:
+        headers:
         options:
         converters:
 
@@ -226,7 +348,7 @@ def from_madx_twiss(filename: str = 'twiss.outx',
     p = getattr(particules, twiss_headers['PARTICLE'].capitalize())
     k = Kinematics(float(twiss_headers['PC']) * _ureg.GeV_c, particle=p)
     converted_table: list = list(
-                      load_madx_twiss_table(filename, path).set_index('NAME').apply(
+                      load_madx_twiss_table(filename, path, headers).set_index('NAME').apply(
                           lambda _: conversion_functions.get(_['KEYWORD'],
                                                              lambda _, __, ___: None
                                                              )(_, k, options.get(_['KEYWORD'], {})),
