396f8b80893b42b534cec0fd85fc29abb1d7a93e
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2019-03-18 15:31:57 +0100

    Bug fix and remove a reference to a local path.

--- zgoubidoo/fieldmaps/fieldmap.py
+++ zgoubidoo/fieldmaps/fieldmap.py
@@ -1,3 +1,4 @@
+from typing import Optional, Union
 import os
 import numpy as np
 import pandas as pd
@@ -5,7 +6,7 @@ from scipy import interpolate
 from lmfit import Model
 
 
-def enge(s,
+def enge(s: Union[float, np.array],
          ce_0: float = 0.0,
          ce_1: float = 1.0,
          ce_2: float = 0.0,
@@ -24,7 +25,34 @@ def enge(s,
          offset_s: float = 1.0,
          amplitude: float = 1.0,
          field_offset: float = 0.0,
-         ):
+         ) -> Union[float, np.array]:
+    """
+    Enge function for the modelling of fringe fields.
+
+    Args:
+        s: the coordinate (can be a numpy array)
+        ce_0: zero-th order coefficient for the entrance fringe
+        ce_1: first order coefficient for the entrance fringe (usually set to 1)
+        ce_2: second order coefficent for the entrance fringe
+        ce_3: third order coefficient for the entrance fringe
+        ce_4: fourth order coefficient for the entrance fringe
+        ce_5: fifth order coefficient for the entrance fringe
+        cs_0: zero-th order coefficient for the exit fringe
+        cs_1: first order coefficient for the exit fringe (usually set to 1)
+        cs_2: second order coefficient for the exit fringe
+        cs_3: third order coefficient for the exit fringe
+        cs_4: fourth order coefficient for the exit fringe
+        cs_5: fifth order coefficient for the exit fringe
+        lam_e: characteristic length of the entrance fringe
+        lam_s: characteristic length of the exit fringe
+        offset_e: offset for the positionning of the entrance fall-off
+        offset_s: offset for the positionning of the exit fall-off
+        amplitude: field amplitude (not necesserally equal to the maximum)
+        field_offset: field offset
+
+    Returns:
+        the value of the Enge function at coordinate s.
+    """
     p_e = ce_0 + ce_1 * (-(s - offset_e) / lam_e) + ce_2 * (-(s - offset_e) / lam_e) ** 2 + ce_3 * (
                 -(s - offset_e) / lam_e) ** 3 + ce_4 * (-(s - offset_e) / lam_e) ** 4 + ce_5 * (
                       -(s - offset_e) / lam_e) ** 5
@@ -34,7 +62,17 @@ def enge(s,
     return amplitude * (1 / (1 + np.exp(p_e))) * (1 / (1 + np.exp(p_s))) + field_offset
 
 
-def load_mesh_data(file: str, path: str = ''):
+def load_mesh_data(file: str, path: str = '.'):  # -> List[np.array]
+    """
+    Load a mesh data file and creates a complete mesh grid using numpy.
+
+    Args:
+        file: the file containing the mesh data
+        path: path to the mesh data file
+
+    Returns:
+        A numpy mesh grid (list of arrays)
+    """
     with open(os.path.join(path, file)) as f:
         _, x_dim, y_dim, z_dim, _, _ = tuple(map(int, f.readline().split()))
         data = np.array(list(map(float, ' '.join(f.readlines()).split())))
@@ -45,16 +83,35 @@ def load_mesh_data(file: str, path: str = ''):
 
 
 def load_field_data(file: str, path: str = '.') -> pd.DataFrame:
-    return pd.read_csv(os.path.join(path, file), sep='\s+', names=['BX', 'BY', 'BZ', 'M'], header=None)
+    """
+
+    Args:
+        file: the file containing the field map data
+        path: path to the field mpa data file
+
+    Returns:
+
+    """
+    return pd.read_csv(os.path.join(path, file), sep=r'\s+', names=['BX', 'BY', 'BZ', 'M'], header=None)
 
 
-def load_fieldmap(field_file: str, mesh_file: str, path: str = '.'):
+def load_fieldmap(field_file: str, mesh_file: str, path: str = '.') -> np.array:
+    """
+
+    Args:
+        field_file:
+        mesh_file:
+        path:
+
+    Returns:
+
+    """
     x, y, z = [c.reshape((np.prod(c.shape),)) for c in load_mesh_data(file=mesh_file, path=path)]
     f = load_field_data(file=field_file, path=path).values.T.reshape((4, np.prod(x.shape)))
     return np.array([x, y, z, *f]).T
 
 
-class FieldMap:
+class CartesianFieldMap:
     def __init__(self, field_file: str, mesh_file: str, path: str = '.'):
         self._df: Optional[pd.DataFrame] = None
         self._data = load_fieldmap(field_file=field_file, mesh_file=mesh_file, path=path)
@@ -94,7 +151,7 @@ class FieldMap:
         }
         return interpolate.griddata(self._data[:, 0:3], field_components[field_component], points, method=method)
 
-    def export():
+    def export(self):
         pass
 
 
