0c7e2e6cfab2b8e7ab5a13958fb5fc34d17d6608
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2019-05-13 14:54:20 +0200

    Adding MAD-X SBEND and RBEND.

--- zgoubidoo/commands/madx/elements.py
+++ zgoubidoo/commands/madx/elements.py
@@ -57,3 +57,80 @@ class Marker(MadElementCommand):
     """
     KEYWORD = 'MARKER'
     """Keyword of the command used for the MAD-X input data."""
+
+
+class BendType(MadElementCommandType):
+    pass
+
+
+class Bend(MadElementCommand, metaclass=BendType):
+    """A base class for the two kinds of BENDs in MAD-X: SBEND and RBEND."""
+    PARAMETERS = {
+        'L': (0 * _ureg.m, 'The length of the magnet (default: 0 m). For sector bends the declared length is the arc '
+                           'length of the reference orbit. For rectangular bends the declared length is normally the '
+                           'length of a straight line joining the entry and exit points, as in the Figure. Internally '
+                           'MAD-X only uses the arc length of the reference orbit for both bend types. In order to '
+                           'define RBEND’s with a declared length equal to the arc length of the reference orbit, the '
+                           'option RBARC must be previously set to FALSE in MAD-X with Option, RBARC=false;'),
+        'ANGLE': (0 * _ureg.radian, 'The bend angle (default: 0 rad). A positive bend angle represents a bend to the '
+                                    'right, i.e. towards negative x values. Please notice that ANGLE is used to define '
+                                    'bend strength, K0. To define the roll angle use TILT.'),
+        'ADD_ANGLE': (None, 'An array of (maximum 5) bending angles for multiple passes. See ADD_PASS option of the '
+                            'SEQUENCE command.'),
+        'TILT': (0 * _ureg.radian, 'The roll angle about the longitudinal axis (default: 0 rad, i.e. a horizontal '
+                                   'bend). A positive angle represents a clockwise rotation. An attribute TILT=pi/2 '
+                                   'turns a horizontal into a vertical bend, and a positive ANGLE then denotes a '
+                                   'downwards deflection.'),
+        'K1': (None, 'The quadrupole coefficient (Default: 0 m−2) K1 = (1/Bρ)(∂By/∂x). A positive quadrupole strength '
+                     'implies horizontal focussing of particles, irre- spective of their charge. K1 is taken into '
+                     'account in thick bend transfer map. Only K0 and K1 are considered for think transfer map, not '
+                     'any other strengths (like K2, K1S).'),
+        'E1': (None, 'The rotation angle for the entrance pole face (Default: 0 rad).'),
+        'E2': (None, 'The rotation angle for the exit pole face (Default: 0 rad).'),
+        'FINT': (0, 'The fringe field integral at entrance and exit of the bend. (Default: 0).'),
+        'FINTX': (None, 'If defined and positive, the fringe field integral at the exit of the element, overriding '
+                        'FINT for the exit (Default: =FINT). This allows to set different fringe field integrals at '
+                        'entrance (FINT) and exit (FINTX) of the element.'),
+        'HGAP': (None, 'The half gap of the magnet (default: 0 m).'),
+        'K2': (None, 'The sextupole coefficient. (Default: 0 m−3). Please note that K2 is not taken into account for '
+                     'bend transfer map.'),
+        'K1S': (None, 'The skew quadrupole coefficient. Ks = 1/(2Bρ)(∂Bx/∂x − ∂By/∂y) where (x,y) is a coordinate '
+                      'system rotated by −45 degrees around s with respect to the normal one. The default is 0 m−2. '
+                      'A positive skew quadrupole strength implies defocussing, irrespective of the charge of the '
+                      'particles, in the (x,s) plane rotated by 45◦ around s (particles in this plane have x = y > 0). '
+                      'Please note that K1S is not taken into account for bend transfer map.'),
+        'H1': (None, 'The curvature of the entrance pole face. (Default: 0 m−1).'),
+        'H2': (None, 'The curvature of the exit pole face. (Default: 0 m−1)'),
+        'K0': (None, 'Please take note that K0 and K0S are left in the data base but are no longer used for the MAP of '
+                     'the bends, instead ANGLE and TILT are used exclusively. However, K0 is assignment of relative '
+                     'field errors to a bending magnet because K0 is used for the normalization instead of the ANGLE. '
+                     '(see EFCOMP). With K0 = (1/Bρ)By, one gets K0 = ANGLE / arclength.'),
+        'THICK': (None, 'If this logical flag is set to true the bending magnet is tracked through as a thick-element, '
+                        'instead of being converted into thin-lenses. (Default: false)'),
+        'KILL_ENT_FRINGE': (None, 'If this logical flag is set to true the fringe fields on the entrance of the '
+                                  'element are not taken into account (not calculated). (Default: false).'),
+        'KILL_EXT_FRINGE': (None, 'If this logical flag is set to true the fringe fields on the exit of the element '
+                                  'are not taken into account (not calculated). (Deefault: false).')
+    }
+    """Parameters of the command, with their default value, their description and optinally an index used by other 
+        commands (e.g. fit)."""
+
+
+class RBend(Bend):
+    """
+    A drift space.
+
+    The straight reference system for a drift space is a Cartesian coordinate system.
+    """
+    KEYWORD = 'RBEND'
+    """Keyword of the command used for the MAD-X input data."""
+
+
+class SBend(Bend):
+    """
+    Sector bending magnet.
+
+    The planes of the pole faces intersect at the centre of curvature of the curved sbend reference system.
+    """
+    KEYWORD = 'SBEND'
+    """Keyword of the command used for the MAD-X input data."""
