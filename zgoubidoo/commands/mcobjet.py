d9ef0d6d276b5e502f2840f8b2440605bb7d4aa3
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2019-06-04 09:24:56 +0200

    Creation of the MCObjet3.

--- zgoubidoo/commands/mcobjet.py
+++ zgoubidoo/commands/mcobjet.py
@@ -3,6 +3,7 @@
 More details here.
 """
 from .commands import Command as _Command
+from .. import ureg as _ureg
 
 
 class MCObjet(_Command):
@@ -11,9 +12,20 @@ class MCObjet(_Command):
     """Keyword of the command used for the Zgoubi input data."""
 
     PARAMETERS = {
-        'BORO': 1.0,
-        'IMAX': 1,
-        'KY': 1,
+        'BORO': (1.0 * _ureg.tesla * _ureg.m, 'Reference rigidity'),
+        'IMAX': (1, 'Number of particles to be generated'),
+        'KY': (1, '1 - Uniform, 2 - Gaussian, 3 - Parabolic'),
+        'KT': (1, '1 - Uniform, 2 - Gaussian, 3 - Parabolic'),
+        'KZ': (1, '1 - Uniform, 2 - Gaussian, 3 - Parabolic'),
+        'KP': (1, '1 - Uniform, 2 - Gaussian, 3 - Parabolic'),
+        'KX': (1, '1 - Uniform, 2 - Gaussian, 3 - Parabolic'),
+        'KD': (1, '1 - Uniform, 2 - Exponential, 3 - Kinematic'),
+        'Y0': (0 * _ureg.cm, 'Mean value'),
+        'T0': (0 * _ureg.rad, 'Mean value'),
+        'Z0': (0 * _ureg.cm, 'Mean value'),
+        'P0': (0 * _ureg.rad, 'Mean value'),
+        'X0': (0 * _ureg.cm, 'Mean value'),
+        'D0': (1, 'Mean value'),
     }
     """Parameters of the command, with their default value, their description and optinally an index used by other 
         commands (e.g. fit)."""
@@ -34,40 +46,32 @@ class MCObjet2(MCObjet):
 
 
 class MCObjet3(MCObjet):
-    """
+    """Monte-Carlo generation of a 6-D object.
 
     """
     PARAMETERS = {
-        'KY': 2,
-        'KT': 1,
-        'KZ': 1,
-        'KP': 1,
-        'KX': 1,
-        'KD': 1,
-        'Y0': 1,
-        'T0': 1.0,
-        'Z0': 1.0,
-        'P0': 1.0,
-        'X0': 1.0,
-        'D0': 1.0,
-        'ALPHA_Y': 0.0,
-        'BETA_Y': 1.0,
-        'EMIT_Y': 1e-9,
-        'N_CUTOFF_Y': 0,
-        'N_CUTOFF2_Y': 0,
-        'ALPHA_Z': 0.0,
-        'BETA_Z': 1.0,
-        'EMIT_Z': 1e-9,
-        'N_CUTOFF_Z': 0,
-        'N_CUTOFF2_Z': 0,
-        'ALPHA_X': 0.0,
-        'BETA_X': 1.0,
-        'EMIT_X': 1e-9,
-        'N_CUTOFF_X': 0,
-        'N_CUTOFF2_X': 0,
-        'I1': 1,
-        'I2': 2,
-        'I3': 3,
+        'ALPHA_Y': (0.0, 'Horizontal (Y) alpha function'),
+        'BETA_Y': (1.0 * _ureg.m, 'Horizontal (Y) beta function'),
+        'EMIT_Y': (1e-9 * _ureg.m * _ureg.radian, 'Horizontal (Y) normalized emittance'),
+        'DY': (0.0 * _ureg.m, 'Horizontal (Y) dispersion'),
+        'DPY': (0.0, 'Horizontal (Y) dispersion prime'),
+        'N_CUTOFF_Y': (10, 'Cut-off value for the horizontal distribution'),
+        'N_CUTOFF2_Y': (0, 'Secondary cut-off value for the horizontal distribution'),
+        'ALPHA_Z': (0.0, 'Vertical (Z) alpha function'),
+        'BETA_Z': (1.0 * _ureg.m, 'Vertical (Z) beta function'),
+        'EMIT_Z': (1e-9 * _ureg.m * _ureg.radian, 'Vertical (Z) normalized emittance'),
+        'DZ': (0.0 * _ureg.m, 'Vertical (Z) dispersion'),
+        'DPZ': (0.0, 'Vertical (Z) dispersion prime'),
+        'N_CUTOFF_Z': (10, 'Cut-off value for the vertical distribution'),
+        'N_CUTOFF2_Z': (0, 'Secondary cut-off value for the vertical distribution'),
+        'ALPHA_X': (0.0, 'Longitudinal (X) alpha function'),
+        'BETA_X': (1.0 * _ureg.m, 'Longitudinal (X) beta function'),
+        'EMIT_X': (1e-9 * _ureg.m * _ureg.radian, 'Longitudinal (X) normalized emittance'),
+        'N_CUTOFF_X': (10, 'Cut-off value for the longitudinal distribution'),
+        'N_CUTOFF2_X': (0, 'Secondary cut-off value for the longitudinal distribution'),
+        'I1': (1, 'Random sequence seed'),
+        'I2': (2, 'Random sequence seed'),
+        'I3': (3, 'Random sequence seed'),
     }
     """Parameters of the command, with their default value, their description and optinally an index used by other 
         commands (e.g. fit)."""
@@ -75,11 +79,13 @@ class MCObjet3(MCObjet):
     def __str__(s) -> str:
         return f"""
         '{s.KEYWORD}' {s.LABEL1} {s.LABEL2}
+        {s.BORO.m_as('kilogauss m'):.12e}
         3
+        {s.IMAX}
         {s.KY} {s.KT} {s.KZ} {s.KP} {s.KX} {s.KD}
-        {s.Y0} {s.T0} {s.Z0} {s.P0} {s.X0} {s.D0}
-        {s.ALPHA_Y} {s.BETA_Y} {s.EMIT_Y} {s.N_CUTOFF_Y} {s.N_CUTOFF2_Y if s.N_CUTOFF_Y < 0 else ''}
-        {s.ALPHA_Z} {s.BETA_Z} {s.EMIT_Z} {s.N_CUTOFF_Z} {s.N_CUTOFF2_Z if s.N_CUTOFF_Z < 0 else ''}
-        {s.ALPHA_X} {s.BETA_X} {s.EMIT_X} {s.N_CUTOFF_X} {s.N_CUTOFF2_X if s.N_CUTOFF_X < 0 else ''}
+        {s.Y0.m_as('cm'):.12e} {s.T0.m_as('radian'):.12e} {s.Z0.m_as("cm"):.12e} {s.P0.m_as('radian'):.12e} {s.X0.m_as('cm'):.12e} {s.D0:.12e}
+        {s.ALPHA_Y:.12e} {s.BETA_Y.m_as('m'):.12e} {s.EMIT_Y.m_as('m radian'):.12e} {s.N_CUTOFF_Y} {s.N_CUTOFF2_Y if s.N_CUTOFF_Y < 0 else ''} {s.DY.m_as('m'):.12e} {s.DPY:.12e}
+        {s.ALPHA_Z:.12e} {s.BETA_Z.m_as('m'):.12e} {s.EMIT_Z.m_as('m radian'):.12e} {s.N_CUTOFF_Z} {s.N_CUTOFF2_Z if s.N_CUTOFF_Z < 0 else ''} {s.DZ.m_as('m'):.12e} {s.DPZ:.12e}
+        {s.ALPHA_X:.12e} {s.BETA_X.m_as('m'):.12e} {s.EMIT_X.m_as('m radian'):.12e} {s.N_CUTOFF_X} {s.N_CUTOFF2_X if s.N_CUTOFF_X < 0 else ''}
         {s.I1} {s.I2} {s.I3}
     """
