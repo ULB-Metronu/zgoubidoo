683c587b7345ecec69eb91eb2ec100fcb0e9ae03
Cédric Hernalsteens <cedric.hernalsteens@iba-group.com>
2019-06-13 10:32:08 +0200

    Refactoring constants to a dedicated file.

---
+++ zgoubidoo/constants.py
@@ -0,0 +1,12 @@
+"""
+Constants
+"""
+
+ZGOUBI_INPUT_FILENAME: str = 'zgoubi.dat'
+"""File name for Zgoubi input data."""
+
+MAD_INPUT_FILENAME: str = 'input.mad'
+"""File name for MAD-X input data."""
+
+ZGOUBI_IMAX: int = 10000
+"""Maximum number of particles that a Zgoubi objet can contain."""
